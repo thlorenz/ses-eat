4cecd5b596614e092364dc23ac8a6d4acd8815cd
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-20 17:29:53 -0400

    "initial repository"

---
+++ client/eat.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var core = require('ses-core');
+var MainView = require('./views/main');
+
+exports.init = function () {
+  core.init();
+  return new MainView({ el: '#ses-eat-main' });
+};
+
